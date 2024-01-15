bd7deaa33fd5021ea709f6d2736336a7c9958cc2
calebeLAR <ca6ele@gmail.com>
2024-01-15 18:16:58 -0300

    trybe.carreira.seção02.dia01

---
+++ section02/dia01_empresas_de_acordo_com_interesse/segment01/Unit01.mjs
@@ -0,0 +1,14 @@
+export default class Unit01 {
+  static title = 'Empresas de acordo com interesse';
+  static topics = [
+    'Empresas de acordo com interesse',
+    'O que é a Cultura de uma empresa e como ela influencia no dia a dia das pessoas?',
+    '1. Desenvolvimento Profissional',
+    '2. Qualidade de Vida',
+    '3. Remuneração e Benefícios',
+    '4. Estabilidade',
+    'Com o que me preocupar antes de me aplicar a uma oportunidade',
+    'Com o que me preocupar após a aplicação? Alinhamento de propósitos e valores',
+    'Atividade Individual: Retomando as empresas listadas',
+  ];
+}
