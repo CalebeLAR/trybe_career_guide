2c2e3c6475c6fadf97d241602fe53042d89f6897
calebeLAR <ca6ele@gmail.com>
2024-02-21 11:49:27 -0300

    trybe.carreira.seção02.dia02.Unit01c.mjs

---
+++ section02/dia02_stalkeando_empresas/segment01/Unit01.mjs
@@ -0,0 +1,27 @@
+export default class Unit01 {
+  static title = 'Stalkeando empresas';
+  static topics = [
+    'Stalkeando Empresas',
+    'O que geralmente acontece?',
+    'O que te leva ao sucesso nos processos seletivos?',
+    'Como stalkear aquela empresa em que você quer entrar',
+    'A. Framework - O que Procurar',
+    'A1. Macroeconomia',
+    'A2. Segmento/Indústria',
+    'A3. Empresa',
+    'A4. Área/Função',
+    'A5. Entrevistador',
+    'B. Ferramentas - Como Procurar',
+    'B1. LinkedIn',
+    'B2. Glassdoor <3',
+    'B3. SlideShare',
+    'B4. Fóruns e Google Groups',
+    'B5. Google Scholar',
+    'B6. Facebook e Redes Sociais',
+    'B7. Site da Empresa + Relação com Investidores',
+    'B8. YouTube',
+    'B9. Google',
+    'Considerações Finais',
+    'Atividades',
+  ];
+}
