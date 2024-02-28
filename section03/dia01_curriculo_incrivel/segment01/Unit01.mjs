f69c059dc83fa61378c85f2e33649ce159ed2411
calebeLAR <ca6ele@gmail.com>
2024-02-28 11:41:51 -0300

    trybe.carreira.seção03.dia02.linkedin_campeao.Unit01.mjs

---
+++ section03/dia01_curriculo_incrivel/segment01/Unit01.mjs
@@ -0,0 +1,21 @@
+export default class Unit01 {
+  static title = 'Como criar um currículo incrível';
+  static topics = [
+    'Como criar um currículo incrível',
+    'Modelo de currículo: aprenda a fazer o seu',
+    'Qual a importância de um bom currículo?',
+    'Como montar um currículo de sucesso?',
+    'Objetivo',
+    'Formação',
+    'Experiência',
+    'Outras atividades e destaques',
+    'Habilidades',
+    'Idiomas',
+    'Referências',
+    'Como fazer a revisão final?',
+    'O que colocar na carta de apresentação?',
+    'Modelo de Currículo - Trybe',
+    'Colocando em prática',
+    'Avalie o conteúdo',
+  ];
+}
