bc05c20038562806af020e39617f6d33fbd84672
calebeLAR <ca6ele@gmail.com>
2024-03-24 10:09:04 -0300

    trybe.carreira.seção02.dia03.mdc_matriz_de_decisao_de_carreira.Unit01.mjs

---
+++ section02/dia03_mdc_matriz_de_decisao_de_carreira/segment01/Unit01.mjs
@@ -0,0 +1,15 @@
+export default class Unit01 {
+    static title = "MDC - Matriz de Decisão de Carreira";
+    static topics = [
+        "MDC - Matriz de Decisão de Carreira",
+        "O que vamos aprender?",
+        "Decisões data-driven e como stalkear empresas",
+        "A Matriz de Decisão de Carreira (MDC)",
+        "Passos 1, 2 e 3",
+        "Passo 4",
+        "Passo 5:",
+        "Passo 6:",
+        "Passo 7",
+        "Entendendo a Matriz de Decisão de Carreira",
+    ];
+}
