1fbd699959b3a6ade99619532326405a59f86064
calebeLAR <ca6ele@gmail.com>
2024-03-24 10:56:52 -0300

    trybe.carreira.seção02.dia04.refletindo_sobre_a_mdc.Unit01.mjs

---
+++ section02/dia04_refletindo_sobre_a_mdc/segment01/Unit01.mjs
@@ -0,0 +1,11 @@
+export default class Unit01 {
+  static title = 'Refletindo sobre a MDC';
+  static topics = [
+    'Refletindo sobre a MDC',
+    'Onde estamos no Processo de Decisão de Carreira?',
+    'Entendendo a Matriz de Decisão de Carreira',
+    'A intuição no meio disso tudo',
+    'Atividade - Teste de Posição',
+    'Reflexão - Como foi meu dia?',
+  ];
+}
